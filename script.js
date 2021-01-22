0c0eb735e443f6eaa23965adcdb933df95ea002e
Müslüm Ercan <30565300+WERCAN@users.noreply.github.com>
2021-01-22 15:30:49 +0300

    countdown-created

---
+++ script.js
@@ -0,0 +1,41 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+
+
+const newYears = '9 April 2021';
+
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = Math.floor(newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+
+
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minutesEl.innerHTML = mins;
+    secondsEl.innerHTML = seconds;
+
+
+
+}
+
+
+
+
+
+
+// intial call
+countdown();
+
+setInterval(countdown, 1000);
